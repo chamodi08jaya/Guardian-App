26812ed91e9dff550659e534407386eaf06e0ac7
sandunsameera <sandunsameera25@gmail.com>
2020-01-16 00:00:02 +0530

    Finalize tabbed screen

--- flutter_app/lib/Screens/core_screen.dart
+++ flutter_app/lib/Screens/core_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/Screens/instruction_screen.dart';
 
 class CoreScreen extends StatefulWidget {
   @override
@@ -8,8 +9,32 @@ class CoreScreen extends StatefulWidget {
 class _CoreScreenState extends State<CoreScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.indigo,
+            title: Text("Guardian app"),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(text: "Home"),
+                Tab(text: "Contact"),
+                Tab(text: "Option"),
+              ],
+            ),
+          ),
+          body: _coreBody(),
+        ));
+  }
+
+  Widget _coreBody() {
+    return TabBarView(
+      children: <Widget>[
+        // InstructionScreen(),
+        Text("fndjf"),
+        Text("fndjf"),
+        Text("fndjf"),
+      ],
     );
   }
 }
